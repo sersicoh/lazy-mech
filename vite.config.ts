0ba61ea64a09add3b7a721d4c604072f9f8fd957
sersicoh <sersicoh@gmail.com>
2025-04-18 13:25:19 +0200

    based path

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { defineConfig } from 'vite';
 import svgr from 'vite-plugin-svgr';
 
 export default defineConfig({
-  base: '/lazy-mech/',
+  base: '/',
   plugins: [react(), svgr()],
   resolve: {
     alias: {
